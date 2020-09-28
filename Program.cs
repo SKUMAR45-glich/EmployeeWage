d3a90a2f6aa7ed1aadcc1fb577a22f656722790f
ABC <abc@gmail.com>
2020-09-29 01:44:48 +0530

    UC 1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            //Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee id Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
